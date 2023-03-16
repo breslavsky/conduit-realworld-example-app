4314e53bbf56f755f324f0c51b8326f04b19878b
breslavsky <breslavsky.anton@gmail.com>
2023-03-16 19:09:04 +0100

    Run CI/CD workflows on GitHub

--- backend/config/config.js
+++ backend/config/config.js
@@ -7,12 +7,12 @@ module.exports = {
     dialect: "postgres",
   },
   test: {
-    username: "root",
-    password: null,
-    database: "database_test",
-    host: "127.0.0.1",
-    dialect: "mysql",
-  },
+    username: "postgres",
+    password: "zyxxyz",
+    database: "conduit",
+    host: "postgres",
+    dialect: "postgres"
+},
   production: {
     username: process.env.PG_USER,
     password: process.env.PG_PASSWORD,
